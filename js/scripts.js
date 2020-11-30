1576f616da657168b8de7b94deeda66af0503945
Jon Bascos <jon@jonbascos.com>
2020-11-29 17:34:48 -0800

    modal is working and is able to be closed when clicking on the X.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // FETCH Code
 
-fetch('https://randomuser.me/api/?results=12')
+fetch('https://randomuser.me/api/?results=12&nat=us')
     .then(console.log('Fetching...'))
     .then(res => res.json())
     .then(data => {
@@ -23,7 +23,7 @@ const displayInfo = (data) => {
         cardDiv.appendChild(cardImgContainer)
         const img = document.createElement('img')
         img.className = 'card-img'
-        img.src=`${data[i].picture.thumbnail}`
+        img.src=`${data[i].picture.medium}`
         img.alt = 'profile picture'
         cardImgContainer.appendChild(img)
         const cardInfoDiv = document.createElement('div')
@@ -47,55 +47,56 @@ const displayInfo = (data) => {
 
 const displayModal = (data) => {
     const body = document.querySelector('body')
-    document.addEventListener('click', e => {
-        if(e.target.classList.contains('card') || e.target.classList.contains('card-img-container') || e.target.classList.contains('card-text') || e.target.classList.contains('card-img') || e.target.classList.contains('card-name')) {
-               for(let i = 0; i < data.length; i++) {
-                 if(e.target.textContent == data[i].email) {
-                     console.log('found them')
-                    const modalContainerDiv = document.createElement('div')
-                    modalContainerDiv.className = 'modal-container'
-                    body.appendChild(modalContainerDiv)
-                    const modalDiv = document.createElement('div')
-                    modalDiv.className = 'modal'
-                    modalContainerDiv.appendChild(modalDiv)
-                    const modalButton = document.createElement('button')
-                    const strong = document.createElement('strong')
-                    modalButton.type = 'button'
-                    modalButton.id = 'modal-close-btn'
-                    modalButton.className = 'modal-close-btn'
-                    strong.innerText = 'X'
-                    modalDiv.appendChild(modalButton)
-                    modalButton.appendChild(strong)
-                    const modalInfoContainerDiv = document.createElement('div')
-                    modalInfoContainerDiv.className = 'modal-info-container'
-                    modalDiv.appendChild(modalInfoContainerDiv)
-                     
-                    const html = `
-                        <img class="modal-img" src="${data[i].picture.medium}" alt="profile picture">
-                        <h3 id="name" class="modal-name cap">${data[i].name.first} ${data[i].name.last}</h3>
-                        <p class="modal-text">${data[i].email}</p>
-                        <p class="modal-text cap">${data[i].location.city}</p>
-                        <hr>
-                        <p class="modal-text">${data[i].cell}</p>
-                        <p class="modal-text">${data[i].location.street.name}, ${data[i].location.city}, ${data[i].location.state} ${data[i].location.postcode}</p>
-                        <p class="modal-text">Birthday: ${data[i].dob.date}</p>
-                    `
-                    modalInfoContainerDiv.insertAdjacentHTML('beforeend', html)
+    let cards = document.querySelectorAll('.card')
+    console.log('from modal: ', cards[0].innerText)
+    console.log(cards.length)
+    for(let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener('click', e => {
+            if(e.target.innerText === cards[i].innerText) {
+                console.log('found a match')
+                const birthday = data[i].dob.date
+                const convertedDOB = `${birthday.slice(5,7)}/${birthday.slice(8,10)}/${birthday.slice(0,4)}`
+                const modalContainerDiv = document.createElement('div')
+                modalContainerDiv.className = 'modal-container'
+                body.appendChild(modalContainerDiv)
+                const modalDiv = document.createElement('div')
+                modalDiv.className = 'modal'
+                modalContainerDiv.appendChild(modalDiv)
+                const modalButton = document.createElement('button')
+                const strong = document.createElement('strong')
+                modalButton.type = 'button'
+                modalButton.id = 'modal-close-btn'
+                modalButton.className = 'modal-close-btn'
+                strong.innerText = 'X'
+                modalDiv.appendChild(modalButton)
+                modalButton.appendChild(strong)
+                const modalInfoContainerDiv = document.createElement('div')
+                modalInfoContainerDiv.className = 'modal-info-container'
+                modalDiv.appendChild(modalInfoContainerDiv)
+                 
+                const html = `
+                    <img class="modal-img" src="${data[i].picture.large}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${data[i].name.first} ${data[i].name.last}</h3>
+                    <p class="modal-text">${data[i].email}</p>
+                    <p class="modal-text cap">${data[i].location.city}</p>
+                    <hr>
+                    <p class="modal-text">${data[i].cell}</p>
+                    <p class="modal-text">${data[i].location.street.name}, ${data[i].location.city}, ${data[i].location.state} ${data[i].location.postcode}</p>
+                    <p class="modal-text">Birthday: ${convertedDOB}</p>
+                `
 
-                    document.querySelector('.gallery').style.zIndex ='2'
+                modalInfoContainerDiv.insertAdjacentHTML('beforeend', html)
 
-                    const closeButton = document.querySelector('button')
-                    const modalContainer = document.querySelector('.modal')
+                // Places the main page behind the modal
+                document.querySelector('.gallery').style.zIndex ='2'
 
-                    closeButton.addEventListener('click', e => {
-                        e.preventDefault()
-                        console.log('close button clicked')
-                        document.querySelector('.gallery').style.zIndex = '0'
-                    })
-                 }
-               }
-        }
+                // Removes the modal completely when closing the modal
+                const closeButton = document.querySelector('button')
+                closeButton.addEventListener('click', e => {
+                    e.preventDefault()
+                    body.removeChild(modalContainerDiv)
+                })
+            }
         })
+    }
 }
-
-
